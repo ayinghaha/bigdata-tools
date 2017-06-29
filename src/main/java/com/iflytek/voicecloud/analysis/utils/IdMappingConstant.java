bf882cd11081cbcedc84ba9d9ec06dbd433ca738
itm-dev <yingliu6@iflytek.com>
2017-06-29 09:45:59 +0800

    [ADD]添加id图谱工具类

---
+++ src/main/java/com/iflytek/voicecloud/analysis/utils/IdMappingConstant.java
@@ -0,0 +1,17 @@
+package com.iflytek.voicecloud.analysis.utils;
+
+/**
+ * Created by liuying on 2017/6/26.
+ */
+public class IdMappingConstant {
+    public static final String TOKEN="token";
+    public static final String VERSION="ver";
+    public static final String START_TIME="start_time";
+    public static final String END_TIME="end_time";
+    public static final String ALL="all";
+    public static final String CID="cid";
+    public static final String IMPORT_ID="import_id";
+    public static final String TASK_TYPE="task_type";
+    public static final String GROUP_ID="group_id";
+    public static final String GROUPS="groups";
+}
